6927daa1c05b35657d9b2322f9dbeeadb6f3e9a0
Daniel Chróst <78411763+235843@users.noreply.github.com>
2022-06-01 21:29:00 +0200

    Delete WektoryRoznejDlugosciException.java

--- WektoryRoznejDlugosciException.java
+++
@@ -1,5 +0,0 @@
-public class WektoryRoznejDlugosciException extends Exception {
-    WektoryRoznejDlugosciException(int s1, int s2){
-        System.out.println("Dlugosc pierwszego wektora = " + s1 + " dlugosc drugiego wektora = " + s2);
-    }
-}
