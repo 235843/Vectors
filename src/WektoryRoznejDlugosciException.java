331199ce0454e2e384e86702f2657b8f899b508b
Daniel Chróst <78411763+235843@users.noreply.github.com>
2022-06-01 21:29:59 +0200

    Add files via upload

---
+++ src/WektoryRoznejDlugosciException.java
@@ -0,0 +1,5 @@
+public class WektoryRoznejDlugosciException extends Exception {
+    WektoryRoznejDlugosciException(int s1, int s2){
+        System.out.println("Dlugosc pierwszego wektora = " + s1 + " dlugosc drugiego wektora = " + s2);
+    }
+}
